0072da20d07ad09b2f474c5859eff978511e6f53
方盖 <huanghonghui@cai-inc.com>
2025-06-13 14:30:05 +0800

    feat: 基础配置修改

--- .dumirc.ts
+++ .dumirc.ts
@@ -4,5 +4,13 @@ export default defineConfig({
   outputPath: 'docs-dist',
   themeConfig: {
     name: 'kawhi-ui',
+    logo: 'https://domain.com/favicon.ico',
+    prefersColor: {
+      default: 'auto',
+      switch: true,
+    },
+    socialLinks: {
+      github: 'https://github.com/HuangHongHui-FE/kawhi-ui',
+    },
   },
 });
